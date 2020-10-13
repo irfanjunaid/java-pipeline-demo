b61c4d98125e22431612fa96762a8f376a93effc
Irfan Junaid <irfanjunaidrockz@gmail.com>
2020-10-13 11:19:52 +0530

    Add script

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+
+java -jar target/${NAME}-${VERSION}.jar
